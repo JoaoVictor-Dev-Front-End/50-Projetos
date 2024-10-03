67d6fcf3249f56c37b3051a8e8071c2d8c6b313e
Joao Victor <victordevfront_end@icloud.com>
2024-10-03 14:05:04 -0300

    50 Projetos em 50 dias

--- Projeto 1/js.js
+++ Projeto 1/js.js
@@ -1,14 +1,43 @@
-const panels = document.querySelectorAll('.painel')
+const panels = document.querySelectorAll('.panel');
+const background = document.querySelector('.background');
 
-panels.forEach(painel => {
-    painel.addEventListener('click', () => {
-        removeActiveclasses()
-        painel.classList.add('active')
-    })
-})
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
 
-function removeActiveclasses() {
-    panels.forEach(painel => {
-        painel.classList.remove('active')
-    })
+    const id = panel.id;
+    switch (id) {
+      case 'bra':
+        background.style.background = 'linear-gradient(to right, #00843D, #FFCC29, #0000CD)';
+        break;
+      case 'mex':
+        background.style.background = 'linear-gradient(to right, #006400, #FFFFFF, #CE1126)';
+        break;
+        case 'ita':
+        background.style.background = 'linear-gradient(to right, #009246, #FFFFFF, #CD212A)';
+        break;
+        case 'chi':
+        background.style.background = 'linear-gradient(to right, #DE2910, #FFDE00)';
+        break;
+        case 'jor':
+        background.style.background = 'linear-gradient(to right, #000000, #FFFFFF, #138808, #CE1126)';
+        break;
+        case 'per':
+        background.style.background = 'linear-gradient(to right, #EF3340, #FFFFFF, #C8102E)';
+        break;
+        case 'ind':
+        background.style.background = 'linear-gradient(to bottom, #FF9933, #FFFFFF, #138808)';
+        break;
+      
+      default:
+        background.style.background = 'linear-gradient(45deg, #FF0000, #00FF00)';
+    }
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
 }
